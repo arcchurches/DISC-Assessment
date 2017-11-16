46306e92b769fd981c3f9aac987069bc8927b803
Todd Lear <todd@deka.gives>
2017-11-16 09:26:49 -0600

    Share panel hides of organization is not setup to share

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -42,17 +42,19 @@
             <div class="alert alert-success">{{ Session::get('alert-success') }}</div>
         @endif
         <div id="results">
-			<div class="row">
-				<div class="col-md-3 well">
-					<form method="post" action="/send">
-						{{ csrf_field() }}
-						<h4 class="text-muted">Share your results with<br>{{ $params['organization']->name }}</h4>
-						<label for="name" required>Name</label>
-						<input class="form-control" name="name">
-						<button class="btn btn-success btn-sm" type="submit"><i class="fa fa-share" aria-hidden="true" onclick="this.disabled=true;"></i> Share</button>
-					</form>
-				</div>
-			</div>
+            @if(!empty($params['organization']->results_email))
+    			<div class="row">
+    				<div class="col-md-3 well">
+    					<form method="post" action="/send">
+    						{{ csrf_field() }}
+    						<h4 class="text-muted">Share your results with<br>{{ $params['organization']->name }}</h4>
+    						<label for="name" required>Name</label>
+    						<input class="form-control" name="name">
+    						<button class="btn btn-success btn-sm" type="submit"><i class="fa fa-share" aria-hidden="true" onclick="this.disabled=true;"></i> Share</button>
+    					</form>
+    				</div>
+    			</div>
+            @endif
 
 			<?php echo $params['display']; ?>
 		</div>
