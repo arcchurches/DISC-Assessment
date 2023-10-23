3363492832b60387e5569e769147299bdb3f9654
Todd Lear <toddlear@gmail.com>
2023-10-23 13:18:32 -0500

    Update notfound.blade.php

--- resources/views/notfound.blade.php
+++ resources/views/notfound.blade.php
@@ -7,13 +7,4 @@
             <p>It looks like we couldn't find the organization you were looking for.</p>
         </div>
     </div>
-
-    <div class="footer navbar-fixed-bottom">
-    	<div class="container-fluid">
-    		<div class="col-xs-12 copyrightbar text-center">
-    			<p>Powered by <a href="https://www.arcchurches.com">ARC</a>
-				&copy; 2000-{{ date('Y') }}. All Rights Reserved</p>
-    		</div>
-    	</div>
-    </div>
 @endsection
